1345170c69a7bbd40d55e56030b55daba68ad306
AdamAbd <adamabd14113@gmail.com>
2022-09-03 10:56:37 +0700

    add user migration and model

---
+++ migrations/20220902133704-create-users.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      google_id: {
+        allowNull: true,
+        type: Sequelize.TEXT,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.ENUM('user', 'admin'),
+        defaultValue: 'user',
+      },
+      verified_email: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      // deleted_at: {
+      //   type: Sequelize.DATE,
+      // },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};

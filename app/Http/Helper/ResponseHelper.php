afb7635b2fd430624ab3b5f8bf31a176b81632db
AdamAbd <adamabd14113@gmail.com>
2022-10-15 16:35:02 +0700

    Update response helper

--- app/Http/Helper/ResponseHelper.php
+++ app/Http/Helper/ResponseHelper.php
@@ -8,78 +8,83 @@ use Carbon\Carbon;
 
 class ResponseHelper
 {
-    public static function respond($message = 'Success', $data = null, $status = ResponseCode::success)
+    public static function respond($message = "Success", $data = null, $statusCode = ResponseCode::success)
     {
         return response()->json([
-            "status" => $status,
+            "code" => $statusCode,
+            "status" => "Success",
             "message" => $message,
             "data" => $data
-        ], $status);
+        ], $statusCode);
     }
 
-    public static function fail($message, $status = 400,)
+    public static function fail($errors, $status, $statusCode = ResponseCode::invalid_request)
     {
-        return ResponseHelper::respond($message, null, $status);
+        return response()->json([
+            "code" => $statusCode,
+            "status" => $status,
+            "errors" => $errors
+        ], $statusCode);
     }
 
-    public static function respondCreated($message, $data)
+    public static function respondCreated($message, $data = null)
     {
         return ResponseHelper::respond($message, $data, ResponseCode::created);
     }
 
-    public static function responDeleted($message, $data)
+    public static function responDeleted($message, $data = null)
     {
         return ResponseHelper::respond($message, $data, ResponseCode::deleted);
     }
 
-    public static function respondUpdated($message, $data)
+    public static function respondUpdated($message, $data = null)
     {
         return ResponseHelper::respond($message, $data, ResponseCode::updated);
     }
 
-    public static function responNoContent($message, $data)
+    public static function responNoContent($message, $data = null)
     {
         return ResponseHelper::respond($message, $data, ResponseCode::no_content);
     }
 
-    public static function failUnauthorized($message = 'Unauthorized')
+    public static function failUnauthorized($errors = null, $status = "Unauthorized")
     {
-        return ResponseHelper::fail($message, ResponseCode::unauthorized);
+        return ResponseHelper::fail($errors, $status, ResponseCode::unauthorized);
     }
 
-    public static function failForbidden($message = 'Forbidden')
+    public static function failForbidden($errors = null, $status = "Forbidden")
     {
-        return ResponseHelper::fail($message, ResponseCode::forbidden);
+        return ResponseHelper::fail($errors, $status, ResponseCode::forbidden);
     }
 
-    public static function failNotFound($message = 'Not Found')
+    public static function failNotFound($errors = null, $status = "Not Found")
     {
-        return ResponseHelper::fail($message, ResponseCode::resource_not_found);
+        return ResponseHelper::fail($errors, $status, ResponseCode::resource_not_found);
     }
 
-    public static function failValidationError($message = 'Bad Request')
+    public static function failValidationError($errors = null, $status = "Bad Request")
     {
-        return ResponseHelper::fail($message, ResponseCode::invalid_data);
+        return ResponseHelper::fail($errors, $status, ResponseCode::invalid_data);
     }
 
-    public static function failResourceExists($message = 'Conflict')
+    public static function failResourceExists($errors = null, $status = "Conflict")
     {
-        return ResponseHelper::fail($message, ResponseCode::resource_exists);
+        return ResponseHelper::fail($errors, $status, ResponseCode::resource_exists);
     }
 
-    public static function failResourceGone($message = 'Gone')
+    public static function failResourceGone($errors = null, $status = "Gone")
     {
-        return ResponseHelper::fail($message, ResponseCode::resource_gone);
+        return ResponseHelper::fail($errors, $status, ResponseCode::resource_gone);
     }
 
-    public static function failTooManyRequests($message = 'Too Many Requests')
+    public static function failTooManyRequests($errors = null, $status = "Too Many Requests")
     {
-        return ResponseHelper::fail($message, ResponseCode::too_many_requests);
+        return ResponseHelper::fail($errors, $status, ResponseCode::too_many_requests);
     }
 
-    public static function failServerError($message = 'Internal Server Error')
+    public static function failServerError($errors = null, $status = "Internal Server Error")
     {
-        return ResponseHelper::fail($message, ResponseCode::server_error);
+        return ResponseHelper::fail($errors, $status, ResponseCode::server_error);
     }
 
     public static function respondWithToken(User $user)
@@ -88,14 +93,14 @@ class ResponseHelper
             //* Creating two types of token
             //* Access Token used for accesing user only API routes with limited time (30 minute)
             //* Refresh Token used for refresh Access Token after 30 minute 
-            $accessToken = $user->createToken('access-token', ['user|accessToken'], Carbon::now()->addMinute(30))->plainTextToken;
-            $refreshToken = $user->createToken('refresh-token', ['user|refreshToken'], Carbon::now()->addDay(30))->plainTextToken;
+            $accessToken = $user->createToken("access-token", ["user|accessToken"], Carbon::now()->addMinute(30))->plainTextToken;
+            $refreshToken = $user->createToken("refresh-token", ["user|refreshToken"], Carbon::now()->addDay(30))->plainTextToken;
 
             //* Creating cookie with Refresh Token and live only 30 day
-            $cookie = cookie('token', $refreshToken, 60 * 24 * 30);
+            $cookie = cookie("token", $refreshToken, 60 * 24 * 30);
 
             //* Return success with data of user and Access Token while sending the cookie
-            return ResponseHelper::respond('Success Login', [
+            return ResponseHelper::respond('Success Login.', [
                 'user' => $user,
                 'access_token' => $accessToken,
             ])->withCookie($cookie);
